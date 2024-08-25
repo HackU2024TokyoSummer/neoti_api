53ced5c72db83dff68a71b81eae3a49f556d047f
Nozomi Fukawa <noppi5959@gmail.com>
2024-08-25 15:14:49 +0900

    aa

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  skip_before_action :verify_token, only: [:create]
+  def create
+    user = User.find_by(email: params['email'])
+
+    if user && user&.authenticate(params[:password])
+      jti = user.jti
+      exp = (Time.now + 1.hour).to_i
+      payload = {jti: jti, user_id: user.id}
+      token = JWT.encode(payload, Rails.application.credentials[:secret_key_base])
+    end
+    render json: {token: token}
+  end
+end
