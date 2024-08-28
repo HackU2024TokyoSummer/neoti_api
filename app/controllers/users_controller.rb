ec2adefdfd6c82b82dc64a4cf23a87a647928b2c
Nozomi Fukawa <noppi5959@gmail.com>
2024-08-29 01:41:22 +0900

    動け！

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_token, only: [:new, :create]
   before_action :set_user, only: %i[ show update destroy ]
 
   # GET /users
@@ -13,14 +14,17 @@ class UsersController < ApplicationController
     render json: @user
   end
 
+  def new
+    @user = User.new
+  end
+
   # POST /users
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: { message: 'User created successfully', user: @user }, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
